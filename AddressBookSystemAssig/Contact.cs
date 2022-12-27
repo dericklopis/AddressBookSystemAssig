f131d837820edb0923cf864984e71d6c56042aa9
dericklopis <stanforever08@gmail.com>
2022-12-27 17:47:46 +0530

    Added: Edit Details of Address Book

--- AddressBookSystemAssig/Contact.cs
+++ AddressBookSystemAssig/Contact.cs
@@ -7,28 +7,40 @@ using System.Threading.Tasks;
 namespace AddressBookSystemAssig
 {
     class Contact
-    {
-        List<Person> person1 = new List<Person>();
-        public void addcontact()
-        {
-            Person person = new Person();
-            Console.Write("Enter First Name: ");
-            person.FirstName = Console.ReadLine();
-            Console.Write("Enter Last Name: ");
-            person.LastName = Console.ReadLine();
-            Console.Write("Enter Phone Number: ");
-            person.PhoneNo = Console.ReadLine();
-            Console.Write("Enter Address : ");
-            Console.Write("Enter Address : ");
-            person.Address = Console.ReadLine();
-            Console.Write("Enter city : ");
-            person.City = Console.ReadLine();
-            Console.Write("Enter pinCode : ");
-            person.ZipCode = Console.ReadLine();
-            Console.Write("Enter EmailId : ");
-            person.EmailId = Console.ReadLine();
-            person1.Add(person);
-            Console.ReadLine();
-        }
+    {       
+            List<Person> person1 = new List<Person>();
+            public void addcontact()
+            {
+                Person person = new Person();
+                Console.Write("Enter First Name: ");
+                person.FirstName = Console.ReadLine();
+                Console.Write("Enter Last Name: ");
+                person.LastName = Console.ReadLine();
+                Console.Write("Enter Phone Number: ");
+                person.PhoneNo = Console.ReadLine();
+                Console.Write("Enter Address : ");
+                Console.Write("Enter Address : ");
+                person.Address = Console.ReadLine();
+                Console.Write("Enter city : ");
+                person.City = Console.ReadLine();
+                Console.Write("Enter pinCode : ");
+                person.ZipCode = Console.ReadLine();
+                Console.Write("Enter EmailId : ");
+                person.EmailId = Console.ReadLine();
+                person1.Add(person);
+                Console.ReadLine();
+            }
+            //UC3-Edit person Details
+            public void EditContact()
+            {
+                Console.WriteLine("Enter the 1st Name");
+                string FirstName = Console.ReadLine();
+                Person person = new Person();
+                person1.Equals(person);
+                Console.WriteLine("Enter a new Name");
+                Console.ReadLine();
+                Console.WriteLine("Contacts are successfully Edited");
+                Console.ReadLine();
+            }
     }
 }
--- AddressBookSystemAssig/Program.cs
+++ AddressBookSystemAssig/Program.cs
@@ -8,10 +8,11 @@ namespace AddressBookSystemAssig
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Contact person = new Contact();
-            person.addcontact();
-        }
+            static void Main(string[] args)
+            {
+                Contact person = new Contact();
+                person.addcontact();
+                person.EditContact();
+            }
     }
 }
