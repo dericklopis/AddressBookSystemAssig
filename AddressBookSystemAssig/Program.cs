5fd17f81795d3d1e53b78112ec6599ac88af41b1
dericklopis <stanforever08@gmail.com>
2022-12-27 17:35:03 +0530

    Added: Creating Address Book

--- AddressBookSystemAssig/Program.cs
+++ AddressBookSystemAssig/Program.cs
@@ -6,13 +6,24 @@ using System.Threading.Tasks;
 
 namespace AddressBookSystemAssig
 {
-        internal class Program
-   {
+    internal class Program
+    {
         static void Main(string[] args)
         {
-              Console.WriteLine("Welcome to Address Book Program");
-            Console.ReadLine();
+            Console.WriteLine("Enter your First Name");
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter your ZipCode");
+            string zipcode = Console.ReadLine();
+            Console.WriteLine("Enter your Email_Id");
+            string emailId = Console.ReadLine();
+            Console.WriteLine("Enter your Phone_Number");
+            string phoneNo = Console.ReadLine();
+            Console.WriteLine($"\n First Name : {firstName}\n Last Name : {lastName}\n Address : {address}\n State : {state}\n ZipCode : {zipcode} \n Email ID : {emailId} \n Phone Number : {phoneNo}");
         }
-   }
-
+    }
 }
