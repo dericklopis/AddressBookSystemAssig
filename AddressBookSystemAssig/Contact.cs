9277a8ba72000e66f1e015f9785f2eedde9bdc74
dericklopis <stanforever08@gmail.com>
2022-12-27 17:41:01 +0530

    Added: Details of Address Book

---
+++ AddressBookSystemAssig/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemAssig
+{
+    class Contact
+    {
+        List<Person> person1 = new List<Person>();
+        public void addcontact()
+        {
+            Person person = new Person();
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNo = Console.ReadLine();
+            Console.Write("Enter Address : ");
+            Console.Write("Enter Address : ");
+            person.Address = Console.ReadLine();
+            Console.Write("Enter city : ");
+            person.City = Console.ReadLine();
+            Console.Write("Enter pinCode : ");
+            person.ZipCode = Console.ReadLine();
+            Console.Write("Enter EmailId : ");
+            person.EmailId = Console.ReadLine();
+            person1.Add(person);
+            Console.ReadLine();
+        }
+    }
+}
--- AddressBookSystemAssig/Program.cs
+++ AddressBookSystemAssig/Program.cs
@@ -10,20 +10,8 @@ namespace AddressBookSystemAssig
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your First Name");
-            string firstName = Console.ReadLine();
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter your Address");
-            string address = Console.ReadLine();
-            Console.WriteLine("Enter your State");
-            string state = Console.ReadLine();
-            Console.WriteLine("Enter your ZipCode");
-            string zipcode = Console.ReadLine();
-            Console.WriteLine("Enter your Email_Id");
-            string emailId = Console.ReadLine();
-            Console.WriteLine("Enter your Phone_Number");
-            string phoneNo = Console.ReadLine();
-            Console.WriteLine($"\n First Name : {firstName}\n Last Name : {lastName}\n Address : {address}\n State : {state}\n ZipCode : {zipcode} \n Email ID : {emailId} \n Phone Number : {phoneNo}");
+            Contact person = new Contact();
+            person.addcontact();
         }
     }
 }
