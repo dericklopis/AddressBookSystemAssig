eb2e034844f6273518020e677e23cef89f4f2dd3
dericklopis <stanforever08@gmail.com>
2022-12-27 17:57:33 +0530

    Added: Multiple Person Details in Address Book

--- AddressBookSystemAssig/Contact.cs
+++ AddressBookSystemAssig/Contact.cs
@@ -9,6 +9,7 @@ namespace AddressBookSystemAssig
     class Contact
     {
         List<Person> person1 = new List<Person>();
+        Dictionary<string, List<Person>> person2 = new Dictionary<string, List<Person>>();
         public void addcontact()
         {
             Person person = new Person();
@@ -19,7 +20,7 @@ namespace AddressBookSystemAssig
             Console.Write("Enter Phone Number: ");
             person.PhoneNo = Console.ReadLine();
             Console.Write("Enter Address : ");
-            Console.Write("Enter Address : ");
+
             person.Address = Console.ReadLine();
             Console.Write("Enter city : ");
             person.City = Console.ReadLine();
@@ -33,14 +34,18 @@ namespace AddressBookSystemAssig
         //UC3-Edit person Details
         public void EditContact()
         {
-            Console.WriteLine("Enter the 1st Name");
-            string FirstName = Console.ReadLine();
-            Person person = new Person();
-            person1.Equals(person);
-            Console.WriteLine("Enter a new Name");
-            Console.ReadLine();
-            Console.WriteLine("Contacts are successfully Edited");
-            Console.ReadLine();
+            Console.WriteLine("Enter the 1st Name to edit the details");
+            string firstName = Console.ReadLine();
+            foreach (var contact in person1)
+            {
+                if (contact.FirstName.Equals(firstName))
+                {
+                    Console.WriteLine("Enter the 1st name to Edit");
+                    contact.FirstName = Console.ReadLine();
+                    Console.WriteLine("Enter the last name to Edit");
+                    contact.LastName = Console.ReadLine();
+                }
+            }
         }
         //UC4-Delete contact Details
         public void RemoveDetails()
@@ -50,9 +55,36 @@ namespace AddressBookSystemAssig
             Console.WriteLine("Enter the last Name");
             string LastName = Console.ReadLine();
             Person person = new Person();
-            person1.Remove(person);
-            Console.WriteLine("Contacts are successfully Deleted");
-            Console.ReadLine();
+            foreach (var contact in person1.ToList())
+            {
+                if (contact.FirstName.Equals(FirstName) && contact.LastName.Equals(LastName))
+                {
+                    Console.WriteLine("RemoveContact" + contact);
+                    person1.Remove(contact);
+                }
+            }
+            //Console.WriteLine("Contacts are successfully Deleted" + person1.Remove(person));                
         }
+        public void displayDetails()
+        {
+            foreach (var contact in person1)
+            {
+                Console.WriteLine(contact.FirstName);
+                Console.WriteLine(contact.LastName);
+                Console.WriteLine(contact.Address);
+                Console.WriteLine(contact.PhoneNo);
+                Console.WriteLine(contact.ZipCode);
+                Console.WriteLine(contact.EmailId);
+            }
+        }
+        // UC5
+        public void CreateDict()
+        {
+            Console.WriteLine("Enter a name here");
+            string name = Console.ReadLine();
+            person2.Add(name, person1);
+            person1 = new List<Person>();
+        }
+       
     }
 }
